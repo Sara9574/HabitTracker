ea805d7d738ddfd6cfa876265a56728369aa5f3f
Sara Gholami <sara.ghm95@gmail.com>
2026-07-30 11:18:58 +0200

    Add AddHabit page with form and validation

--- HabitTracker.Web/Models/Habit.cs
+++ HabitTracker.Web/Models/Habit.cs
@@ -1,11 +1,17 @@
-﻿namespace HabitTracker.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace HabitTracker.Web.Models
 {
     public class Habit
     {
         public int Id { get; set; }
-        public string Name { get; set; } = "";
+
+        [Required] 
+        public string Name { get; set; } = string.Empty;
+
+        [Range(1, 100)]
         public int WeeklyGoal { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime CreatedAt { get; set; }
 
         public List<HabitCompletion> Completions { get; set; } = new();
     }
